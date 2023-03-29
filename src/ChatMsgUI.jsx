10f25cbbbd98c46a5e60f041998c11f7374b2180
RohanKGP <meshramrohan786@gmail.com>
2023-03-29 20:38:47 +0530

    completed

--- src/ChatMsgUI.jsx
+++ src/ChatMsgUI.jsx
@@ -2,13 +2,10 @@ import React, { useEffect, useState } from "react";
 import "./index.css";
 import { createClient } from "@supabase/supabase-js";
 
-// Todo: Keys for Supabase
-
-const VITE_SUPABASE_KEY =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inhwbm1rZ3FmbHVqcnhva2FnY3RjIiwicm9sZSI6ImFub24iLCJpYXQiOjE2Nzk5Mzk3OTksImV4cCI6MTk5NTUxNTc5OX0.TUvQV7cmSvh9PHq38-fQPqpYO8zoBRku8Ml_-5Ra2Zw";
-const VITE_SUPABASE_URL = "https://xpnmkgqflujrxokagctc.supabase.co";
-
-const supabase = createClient(VITE_SUPABASE_URL, VITE_SUPABASE_KEY);
+const supabase = createClient(
+  import.meta.env.VITE_SUPABASE_URL,
+  import.meta.env.VITE_SUPABASE_KEY
+);
 
 var channel;
 
